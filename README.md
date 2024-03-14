cca83012cba199396ded3673a2cd5b2d92dcc70f
Steve2472348508 <Steve2472348508@users.noreply.github.com>
2024-03-14 08:55:44 +0000

    deploy: 0c30a8359292b20a0b3a8cdc816f43598a690de3

--- README.md
+++
@@ -1,7 +0,0 @@
-## Planet-UI
-🚀一个基于vite+vue3的组件库，用于练习开源库打包
-
-### 技术栈
-- ⚡Vite
-- 📐TypeScript
-- 🌏vue3
--- docs/.vitepress/theme/index.ts
+++
@@ -1,8 +0,0 @@
-import type { Theme } from 'vitepress'
-import VPTheme from 'vitepress/theme'
-
-import './custom.css'
-
-export default {
-	...VPTheme,
-} as Theme
